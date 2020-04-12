ed418cc5422409046ee6ef785a4a19c2bf504783
Tom Selfin <selfint@gmail.com>
2020-04-12 21:04:19 +0300

    finish ex 1

---
+++ src/ex_1.rs
@@ -0,0 +1,68 @@
+/*
+Given a list of integers, use a vector and return the mean (the average value), median (when sorted, the value in the middle position), and mode (the value that occurs most often; a hash map will be helpful here) of the list.
+*/
+use std::collections::HashMap;
+
+pub fn get_mean(integers: &Vec<i32>) -> f32 {
+
+    // get sum of integers
+    let mut sum = 0;
+    for number in integers {
+        sum += number;
+    }
+
+    // return average
+    (sum / integers.len() as i32) as f32
+}
+
+pub fn get_median(integers: &Vec<i32>) -> i32 {
+
+    // clone vec
+    let mut clone = integers.clone();
+
+    // sort clone using bubble sort
+    for _ in 0..clone.len() {
+        for j in 1..clone.len() {
+            if clone[j-1] > clone[j] {
+                clone.swap(j-1, j);
+            }
+        }
+    }
+
+    // if there is an even amount of integers, return the avg of the two middle ones
+    // else return the middle one
+    if clone.len() % 2 == 0 {
+        (clone[clone.len() / 2] - clone[clone.len() / 2 - 1]) / 2
+    } else {
+        clone[clone.len() / 2]
+    }
+}
+
+pub fn get_mode(integers: &Vec<i32>) -> i32 {
+    let mut number_counter: HashMap<i32, u32> = HashMap::new();
+
+    // count numbers in vec
+    for number in integers {
+        let count = number_counter.entry(*number).or_insert(0);
+        *count += 1;
+    }
+
+    // get the count of the number that appeard the most
+    let mut max_count: u32 = 0;
+    for count in number_counter.values() {
+        if count > &max_count {
+            max_count = *count;
+        }
+    }
+
+    // get that number (if there is more than one, so be it who cares)
+    for (number, count) in number_counter {
+        if count == max_count {
+            return number;
+        }
+    }
+
+    // fail silently, maybe when I learn more I can fix this
+    // TODO: fix this
+    0
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use std::io::{self, Write};
+mod ex_1;
+
 fn main() {
-    
+    let list_of_integers = get_integers();
+    let mean = ex_1::get_mean(&list_of_integers);
+    let median = ex_1::get_median(&list_of_integers);
+    let mode = ex_1::get_mode(&list_of_integers);
+    println!("The mean is: {}", mean);
+    println!("The median is: {}", median);
+    println!("The mode is: {}", mode);
+}
+
+fn get_integers() -> Vec<i32> {
+    // get amount of integers from user
+    println!("How many numbers do you wish to enter?");
+    let mut amount = String::new();
+    io::stdin()
+        .read_line(&mut amount)
+        .expect("Failed to read line");
+
+    // convert string to int
+    let amount: u32 = amount.trim().parse().expect("Failed to convert to u32");
+
+    // get integers from user
+    let mut integers: Vec<i32> = vec![];
+    for i in 0..amount {
+        let mut number = String::new();
+
+        // get integer from user
+        print!("Enter number {}: ", i);
+        io::stdout().flush().unwrap(); // flush to stdout since we used print! and not println!
+        io::stdin()
+            .read_line(&mut number)
+            .expect("Failed to read line");
+        let number: i32 = number.trim().parse().expect("Failed to convert to u32");
+        integers.push(number);
+    }
+
+    integers
 }

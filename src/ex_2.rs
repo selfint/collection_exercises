6c101dc5f7c6380fa70979594371a79f732d7d3e
Tom Selfin <selfint@gmail.com>
2020-04-13 13:34:37 +0300

    finish ex 2

--- src/ex_2.rs
+++ src/ex_2.rs
@@ -2,14 +2,21 @@
 Convert strings to pig latin. The first consonant of each word is moved to the end of the word and “ay” is added, so “first” becomes “irst-fay.” Words that start with a vowel have “hay” added to the end instead (“apple” becomes “apple-hay”). Keep in mind the details about UTF-8 encoding!
 */
 pub fn piggify(word: &String) -> String {
-    // generate new word
     let mut piggified_word = String::new();
 
-    // check if the first char exists, if not return the word (it is empty)
-    let mut char_iterator = word.chars();
-    match char_iterator.next() {
+    // copy the rest of the word into piggified word
+    for consonant in word.chars().skip(1) {
+        // ignore newlines
+        if consonant != '\n' {
+            piggified_word.push(consonant);
+        }
+    }
+
+    // check if the first char is a vowel or not
+    match word.chars().next() {
         Some(c) => {
             if "aeiou".contains(c) {
+                piggified_word.insert(0, c);
                 piggified_word += "-hay";
             } else {
                 piggified_word += &("-".to_string() + &c.to_lowercase().to_string() + "ay");
@@ -17,13 +24,6 @@ pub fn piggify(word: &String) -> String {
         }
         None => return word.to_string() + "ay",
     };
-
-    // copy the rest of the word into piggified word
-    for consonant in char_iterator.rev() {
-        if consonant != '\n' {
-            piggified_word.insert(0, consonant);
-        }
-    }
 
     piggified_word
 }
